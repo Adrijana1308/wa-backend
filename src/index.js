86814a508b60ed010cce185fb97e26f50c4236cf
Adrijana1308 <adrijanababic4@gmail.com>
2024-02-26 00:16:35 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+import express from "express";
+import storage from "./memory_storage.js";
+
+const app = express();
+const port = 3000;
+
+app.get("/posts", (req, res) => {
+  res.json(storage.posts);
+});
+
+app.listen(port, () => console.log(`Slušam na portu ${port}!`));
---
+++ src/memory_storage.js
@@ -0,0 +1,29 @@
+let storage = {
+    posts: [
+    {
+    id: 10001,
+    source: "https://picsum.photos/id/669/500/500",
+    name: "Salon Stil",
+    location: "Zagreb",
+    date: "46346336",
+    time: "15:00",
+    },
+    {
+    id: 10002,
+    source: "https://picsum.photos/id/669/500/500",
+    name: "Salon Beauty",
+    location: "Zagreb",
+    date: "346347347",
+    time: "15:00",
+    },
+    {
+    id: 10003,
+    source: "https://picsum.photos/id/669/500/500",
+    name: "Salon Barbara",
+    location: "Pula",
+    date: "4577868",
+    time: "15:00",
+    }
+    ]
+   }
+   export default storage;
