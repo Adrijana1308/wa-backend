bcf01443237c3be99c850810063911bb59fc0549
Ivan Sviben <ivan.sviben97@gmail.com>
2024-02-28 23:10:32 +0100

    Neznam koje su promjene, trazi me commit prije pula

--- src/memory_storage.js
+++ src/memory_storage.js
@@ -1,29 +1,104 @@
 let storage = {
-    posts: [
+  posts: [
     {
-    id: 10001,
-    source: "https://picsum.photos/id/669/500/500",
-    name: "Salon Stil",
-    location: "Zagreb",
-    date: "46346336",
-    time: "15:00",
+      id: 10001,
+      source:
+        "https://images.unsplash.com/photo-1580618672591-eb180b1a973f?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Stil",
+      location: "Zagreb",
+      date: "46346336",
+      time: "15:00",
+      numOfRatings: 10,
+      rating: 5,
     },
     {
-    id: 10002,
-    source: "https://picsum.photos/id/669/500/500",
-    name: "Salon Beauty",
-    location: "Zagreb",
-    date: "346347347",
-    time: "15:00",
+      id: 10002,
+      source:
+        "https://images.unsplash.com/photo-1600948836101-f9ffda59d250?q=80&w=1736&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Beauty",
+      location: "Zagreb",
+      date: "346347347",
+      time: "15:00",
+      numOfRatings: 30,
+      rating: 4,
     },
     {
-    id: 10003,
-    source: "https://picsum.photos/id/669/500/500",
-    name: "Salon Barbara",
-    location: "Pula",
-    date: "4577868",
-    time: "15:00",
-    }
-    ]
-   }
-   export default storage;
+      id: 10003,
+      source:
+        "https://images.unsplash.com/photo-1562322140-8baeececf3df?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Barbara",
+      location: "Pula",
+      date: "4577868",
+      time: "15:00",
+      numOfRatings: 1,
+      rating: 4,
+    },
+    {
+      id: 10004,
+      source:
+        "https://images.unsplash.com/photo-1560066984-138dadb4c035?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Borboleta",
+      location: "Osijek",
+      date: "46346336",
+      time: "15:00",
+      numOfRatings: 14,
+      rating: 5,
+    },
+    {
+      id: 10005,
+      source:
+        "https://images.unsplash.com/photo-1470259078422-826894b933aa?q=80&w=1774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Rose",
+      location: "Zagreb",
+      date: "346347347",
+      time: "15:00",
+      numOfRatings: 7,
+      rating: 3,
+    },
+    {
+      id: 10006,
+      source:
+        "https://images.unsplash.com/photo-1634449571010-02389ed0f9b0?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Cash",
+      location: "Varaždin",
+      date: "4577868",
+      time: "15:00",
+      numOfRatings: 6,
+      rating: 5,
+    },
+    {
+      id: 10007,
+      source:
+        "https://images.unsplash.com/photo-1626379501846-0df4067b8bb9?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Trovat",
+      location: "Zagreb",
+      date: "46346336",
+      time: "15:00",
+      numOfRatings: 15,
+      rating: 3,
+    },
+    {
+      id: 10008,
+      source:
+        "https://images.unsplash.com/photo-1613754773306-532ec48b0de5?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Lozcano",
+      location: "Pula",
+      date: "346347347",
+      time: "15:00",
+      numOfRatings: 30,
+      rating: 5,
+    },
+    {
+      id: 10009,
+      source:
+        "https://images.unsplash.com/photo-1521590832167-7bcbfaa6381f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Salon Petri",
+      location: "Zagreb",
+      date: "4577868",
+      time: "15:00",
+      numOfRatings: 32,
+      rating: 4,
+    },
+  ],
+};
+export default storage;
