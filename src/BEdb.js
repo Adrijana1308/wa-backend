0d66f26e1e62d2d0cf0a0b74f847b3969b96e3ce
Ivan Sviben <ivan.sviben97@gmail.com>
2024-03-08 09:25:40 +0100

    Ispravljene greske sa filevoima

--- src/BEdb.js
+++ src/BEdb.js
@@ -1,4 +1,5 @@
 import {MongoClient} from 'mongodb';
+//import {Service, Posts} from '.serviceIndex';
 
 let connection_string = 
     'mongodb+srv://admin:admin@atlascluster.m5jen8k.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster';
@@ -15,14 +16,13 @@ export default () => {
         }
         
         client.connect(err => {
-            if(err){
+            if (err) {
                 reject("Došlo je do greške!" + err)
-            }
-            else{
+            } else {
                 console.log("Uspješno spajanje na bazu!");
                 db = client.db("LocksifyDB")
                 resolve(db)
             }
         })
     })
-};
+ };
