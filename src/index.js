65305dda13feaddc67e75811c649314edd1a257b
Adrijana1308 <adrijanababic4@gmail.com>
2024-03-17 16:29:00 +0100

    pokusaj dohvacanja podataka za Card.vue

--- src/index.js
+++ src/index.js
@@ -4,52 +4,51 @@ import storage from "./memory_storage.js";
 import connect from "./db.js";
 //import * as res from 'express/lib/response';
 
-const app= express(); //instanciranje aplikacije
+const app = express(); //instanciranje aplikacije
 const port = 3000; // port na kojem će web server slušati
 
 app.use(cors()); //zahtjevi mogu biti poslati iz drugih domen
 app.use(express.json()); //zahtjevi mogu biti u JSON formatu, tj. dekodiraj ih u JSON
 
 connect()
-    .then(() => {
-      console.log("Uspješno spajanje na bazu!");
-    })
-      .catch((err) => {
-        console.error("Došlo je do greške prilikom spajanja na bazu!");
-      });
+  .then(() => {
+    console.log("Uspješno spajanje na bazu!");
+  })
+  .catch((err) => {
+    console.error("Došlo je do greške prilikom spajanja na bazu!");
+  });
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
   );
   res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content Type, Accept, Authorization"
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content Type, Accept, Authorization"
   );
   next();
 });
 
-app.post('/posts', async (req, res) =>{
-  try{
-  let db = await connect();
-  let data = req.body; //podaci koji su poslani kroz HTTP zahtjev
-  let result = await db.collection("posts").insertOne(data);
-  res.json(result.ops[0]);
-  } catch (err){
+app.post("/posts", async (req, res) => {
+  try {
+    let db = await connect();
+    let data = req.body; //podaci koji su poslani kroz HTTP zahtjev
+    let result = await db.collection("posts").insertOne(data);
+    res.json(result.ops[0]);
+  } catch (err) {
     console.error("Greska pri umetanju posta: ", err);
-    res.status(500).json({error: "Server error"});
+    res.status(500).json({ error: "Server error" });
   }
   //dodaj u našu bazu (lista u memoriji)
   storage.posts.push(data);
-  
+
   //vrati ono što je spremljeno
   res.json(data); //vrati odgovor klijentu tj. podatke za referencu
 });
 
-app.get('/posts', async (req, res) => {
-
+app.get("/posts", async (req, res) => {
   let db = await connect();
 
   let results;
@@ -60,7 +59,7 @@ app.get('/posts', async (req, res) => {
     console.log(e);
   }
   res.json(results);
-})
+});
 
 app.get("/GetPosts", async (req, res) => {
   let db = await connect();
@@ -77,29 +76,37 @@ app.get("/GetPosts", async (req, res) => {
   res.json(results);
 });
 
-app.get('/posts_memory', (req, res) => {
+app.get("/posts_memory", (req, res) => {
   let posts = storage.posts;
   let query = req.query;
-  
+
   if (query.title) {
-    posts = posts.filter(e => e.title.indexOf(query.title) >= 0);
+    posts = posts.filter((e) => e.title.indexOf(query.title) >= 0);
   }
-  
+
   if (query.createdBy) {
-    posts = posts.filter(e => e.createdBy.indexOf(query.createdBy) >= 0);
+    posts = posts.filter((e) => e.createdBy.indexOf(query.createdBy) >= 0);
   }
-  
-  if(query._any){
-    let terms = query._any.split(' ');
-    posts = posts.filter(doc => {
-      let info=doc.title + ' ' + doc.createdBy; 
-      return terms.every(term => info.indexOf(term) >= 0); 
-    })
+
+  if (query._any) {
+    let terms = query._any.split(" ");
+    posts = posts.filter((doc) => {
+      let info = doc.title + " " + doc.createdBy;
+      return terms.every((term) => info.indexOf(term) >= 0);
+    });
   }
-  
+
   //sortiranja
   posts.sort((a, b) => b.postedAt - a.postedAt);
   res.json(posts);
-})
+});
+
+app.get("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  const db = await connect();
+  const post = await db.collection("posts").findOne({ _id: id });
+  console.log("Post: " + post);
+  res.json(post);
+});
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`));
